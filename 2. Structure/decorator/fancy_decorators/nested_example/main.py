5817a753606576a04aad8812dbb831c3b571c906
Ray <q8977452@gmail.com>
2021-06-02 18:56:37 +0800

    feat(decorator): fancy_decorators folder

---
+++ 2. Structure/decorator/fancy_decorators/nested_example/main.py
@@ -0,0 +1,44 @@
+import functools
+
+
+def debug(func):
+    """Print the function signature and return value"""
+
+    @functools.wraps(func)
+    def wrapper_debug(*args, **kwargs):
+        args_repr = [repr(a) for a in args]  # 1
+        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]  # 2
+        signature = ", ".join(args_repr + kwargs_repr)  # 3
+        print(f"Calling {func.__name__}({signature})")
+        value = func(*args, **kwargs)
+        print(f"{func.__name__!r} returned {value!r}")  # 4
+        return value
+
+    return wrapper_debug
+
+
+def do_twice(func):
+    @functools.wraps(func)
+    def wrapper_do_twice(*args, **kwargs):
+        func(*args, **kwargs)
+        return func(*args, **kwargs)
+
+    return wrapper_do_twice
+
+
+@debug
+@do_twice
+def greet1(name):
+    print(f"Hello {name}")
+
+
+@do_twice
+@debug
+def greet2(name):
+    print(f"Hello {name}")
+
+
+if __name__ == '__main__':
+    greet1("Ray")
+    print('-' * 10)
+    greet2("Ray")
