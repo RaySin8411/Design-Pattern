11b506ed2ed3ebffed1a6aec43cf37c321c04618
Ray <q8977452@gmail.com>
2021-06-03 23:16:15 +0800

    feat(facade): add car and basic examples

---
+++ 2. Structure/facade/car/main.py
@@ -0,0 +1,18 @@
+from car import Car
+
+
+def main():
+    car = Car()
+    car.start()
+    car.drive()
+    car.switch_fog_lights("ON")
+    car.switch_fog_lights("OFF")
+    car.park()
+    car.fill_up_tank()
+    car.drive()
+    car.start()
+    car.drive()
+
+
+if __name__ == "__main__":
+    main()
